8266a2d41584d11ec26536cefb22c7a3a06428f1
tamaskornel <113334847+tamaskornel@users.noreply.github.com>
2023-10-09 13:06:43 +0200

    Add files via upload

---
+++ szamonkeres01.py
@@ -0,0 +1,15 @@
+jegy = int(input(f"kérek egy jegyet egytől ötig:"))
+
+if jegy == 5:
+    print("a jegyed kitűnő!")
+if jegy == 4:
+    print("a jegyed jó!")
+if jegy == 3:
+    print("a jegyed közepes!")
+if jegy == 2:
+    print("a jegyed elégséges!")
+if jegy == 1:
+    print("a jegyed elégtelen!")
+if jegy > 5:
+    print("a jegyed mnem megfelelő!")
+
